12cf5315f795c31eb88508d045b2bbb14af06907
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-09-26 14:29:40 +0800

    再次重新整理內容

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Multimedia-DataBase
 AVI + ACC ，音訊會大量的延展
 MP4 + ACC ，音訊會斷斷續續
 MP4 + MP3 ，音訊正常，需要調整時間的延遲
+
+參照Git
+但不一定一樣，會用Python做運作的方式
+可以利用Python產生對應的資料
--- dshow.bat
+++ dshow.bat
@@ -1,2 +1,2 @@
 ﻿chcp 65001
-ffmpeg -f dshow -i video="HD TV CAM":audio="麥克風 (HD TV CAM)" -max_muxing_queue_size 1024 -init_hw_device cuda -c:v h264_nvenc -c:a mp3 -b:v 9600k -ab 512k -ar 44.1k -r 30 -s 720x480 -aspect 4:3 0721.mp4 -pass 2 -preset veryslow -tune zerolatency
+ffmpeg -f dshow -i video="HD TV CAM":audio="麥克風 (HD TV CAM)" -max_muxing_queue_size 1024 -init_hw_device cuda -c:v h264_nvenc -c:a mp3 -b:v 9600k -ab 512k -ar 44.1k -r 30 -s 720x480 -aspect 4:3 object/0721.mp4 -pass 2 -preset veryslow -tune zerolatency
