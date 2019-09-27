2bbf1a2d9848535fe6cb7ae1bba163d40ea649bd
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-09-27 10:33:15 +0800

    其實發現這些檔案可以移動到暫存資料夾裡面

--- film_merger.bat
+++
@@ -1,1 +0,0 @@
-ffmpeg -init_hw_device cuda -c:v  h264_cuvid -hwaccel cuda -i input\0725.mp4 -init_hw_device cuda -c:v h264_nvenc -b:v 4400k -ab 512k -ar 48000 -r 30 -s 720x480 -aspect 4:3 output\0725.mp4
