1065ac3ddafb6383dd19bb77442bd82a75c6afcf
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-11-21 18:41:38 +0800

    如果採用，此提交將完成基本雜湊功能，目前卡在無法取用
    修改功能:
    出現以下問題:
    ```
    TypeError: '_hashlib.HASH' object is not callable
    ```
    可能有遇過但忘記什麼原因。

--- multimedia_summary.py
+++ multimedia_summary.py
@@ -17,7 +17,7 @@ class HashMultimedia():
     提供讀取、寫入、輸出等自動化操作，並提供API使用.
     """
 
-    def __init__(self, hash_function=None):
+    def __init__(self, hash_function=None, encoding=None, mode="rb", size=-1):
         """
         初始化類別.
 
@@ -28,20 +28,13 @@ class HashMultimedia():
         else:
             self.hash_function = hash_function
 
-    def save_file(self, text, path='../.temp/', encoding='utf8'):
-        """
-        雜湊文字存檔.
-
-        針對輸入的文字雜湊儲存成雜湊值名稱檔案。
-        輸入文字、存檔路徑、編碼格式轉成沒有檔名為雜湊值且沒有副檔名。
-        """
-        hash_function = self.hash_function
-        filename = hash_function(text.encode(encoding)).hexdigest()
-        file = open(path + filename, 'w', encoding=encoding)
-        file.write(text)
+        self.encoding = encoding
+        self.mode = mode
+        self.size = size
 
     @classmethod
-    def read_file(cls, path='./None.mp4', encoding='utf8', mode='r', size=-1):
+    def read_file(cls, path='./.temp/None.mp4',
+                  encoding=None, mode='rb', size=-1):
         """
         讀取文件檔案並計算雜湊值.
 
@@ -50,67 +43,78 @@ class HashMultimedia():
         text = open(path, mode=mode, encoding=encoding)
         return text.read(size)
 
+    def save_file(self, text, path='./.temp/', encoding=None, mode='wb'):
+        """
+        雜湊二進位檔案後存檔.
+
+        針對輸入的文字雜湊儲存成雜湊值名稱檔案。
+        輸入文字、存檔路徑、編碼格式轉成沒有檔名為雜湊值且沒有副檔名。
+        """
+        hash_function = self.hash_function
+        filename = hash_function(text).hexdigest()
+        file = open(path + filename, mode=mode, encoding=encoding)
+        file.write(text)
+
     @classmethod
-    def remove_multimedia(cls, path="./.temp/"):
+    def scan_folder(cls, path='./.temp/'):
+        """
+        掃描目錄底下的檔案名稱並回傳.
+
+        針對指定的目錄位置底下掃描檔案，回傳序列。
+        """
+        multimedias = os.listdir(path)
+        return multimedias
+
+    @classmethod
+    def remove_file(cls, path="./.temp/None.mp4"):
         """
         刪除指定路徑的目錄內檔案.
 
         預設是.temp目錄底下刪除。
         """
-        multimedia_list = os.listdir(path)
-        for multimediafile in multimedia_list:
-            os.remove(path + multimediafile)
+        os.remove(path)
 
-    def read_multimedia_dir(
-            self, path='./.temp/', encoding=None, mode="rb", size=1):
+    def remove_folder(self, path="./.temp/"):
         """
-        讀取指定路徑底下檔案的檔名.
+        刪除指定路徑的目錄內檔案.
 
-        讀取指定的多媒體時會以二進位的方式讀取，當然還是可以決定使用的編碼方式。
+        預設是.temp目錄底下刪除。
         """
-        multimedia = list()
-        multimedias = os.listdir(path)
-        for media in multimedias:
-            media = self.read_file(path=path + media,
-                                   encoding=encoding, mode=mode, size=size)
-            multimedia.append(media)
-        return multimedia
-
-    def read_multimedia_hash(
-            self, path='./.temp/', encoding=None, mode="rb", size=1):
-        """
-        讀取指定檔案或指定路徑底下的多媒體.
-
-        讀取指定路徑底下檔案的檔名.
-
-        讀取指定的多媒體時會以二進位的方式讀取，當然還是可以決定使用的編碼方式。
-        讀取指定的多媒體時會因為每一種使用的編碼都不同，因此會使用二進位的方式讀取，來
-        解決這個問題，但因為可能檔案過於龐大，因此會需要一些方式解決。
-        """
-        multimedia = list()
-        if path[-1:] == "/":
-            multimedias = os.listdir(path)
-            for media in multimedias:
-                media = self.read_file(path=path + media,
-                                       encoding=encoding, mode=mode, size=size)
-                multimedia.append(media)
-        else:
-            multimedia.append(media)
-        return multimedia
-
-    def save_multimedia(self, input_path='./.temp/',
-                        multimedias=None, encoding=None, mode="wb", size=1):
-        """
-        將檔案搭配檔案的雜湊值檔名存入.
-
-        檔案名稱串列或Tuple去尋找目錄底下的檔案，經過雜湊計算後將檔案放入指定的路徑底下
-        取指定的多媒體時會因為每一種使用的編碼都不同，因此會使用二進位的方式讀取，來
-        解決這個問題，但因為可能檔案過於龐大，因此會需要一些方式解決。
-        """
-        if multimedias is None:
-            multimedias = []
-
+        multimedias = self.scan_folder(path=path)
         for multimedia in multimedias:
-            multimedia = self.read_file(path=input_path + multimedia,
-                                        encoding=encoding, mode=mode,
-                                        size=size)
+            self.remove_file(path + multimedia)
+
+    def multimedia_hash(self, input_path='./.temp/None.mp4',
+                        output_path='./.mmdb/object/'):
+        """
+        雜湊指定的多媒體.
+
+        讀取指定的多媒體時會以二進位的方式讀取，並將檔案以雜湊函數計算後的雜湊值製作成
+        檔名，並同時取消副檔名。
+        """
+        self.mode = 'rb'
+        multimedia = self.read_file(path=input_path,
+                                    encoding=self.encoding,
+                                    mode=self.mode,
+                                    size=self.size)
+        self.mode = 'wb'
+        self.save_file(text=multimedia,
+                       path=output_path,
+                       encoding=self.encoding,
+                       mode=self.mode)
+
+    def multimedia_folder_hash(self, input_path='./.temp/',
+                               output_path='./.mmdb/object/'):
+        """
+        自動化目錄雜湊.
+
+        指定路徑讀取目錄底下檔案再指定目錄轉出以檔案雜湊值為檔名的檔案.
+        """
+        multimedias = self.scan_folder(path=input_path)
+        for multimedia in multimedias:
+            self.multimedia_hash(input_path=input_path + multimedia,
+                                 output_path=output_path)
+
+
+HM = HashMultimedia()
+HM.multimedia_folder_hash()
