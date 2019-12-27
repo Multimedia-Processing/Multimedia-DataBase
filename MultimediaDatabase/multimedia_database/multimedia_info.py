221a126e389ce40630b1187d7ae10fe4bc98d8ca
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-12-27 17:43:52 +0800

    如果採用，此提交將部份函式轉移到新的模組內，此模組用於控制資料庫

--- MultimediaDatabase/multimedia_database/multimedia_info.py
+++ MultimediaDatabase/multimedia_database/multimedia_info.py
@@ -3,10 +3,6 @@
 
 將不同類型的媒體讀取資訊後儲存在物件區並搭配上資料表
 """
-
-
-import csv
-import yaml
 from MediaInfo import MediaInfo
 from .multimedia_summary import HashMultimedia
 
@@ -49,62 +45,7 @@ class MultimediaInfo():
         for multimedia in multimedias:
             self.read_file_info(path + multimedia)
 
-    def save_info_yaml(self, media_path='../.temp/None.mp4',
-                       save_path='../.mmdb/info/'):
-        """
-        儲存媒體資訊.
-
-        將指定媒體在讀取資訊後將YAML儲存到指定的目錄底下。
-        """
-        data = self.read_file_info(path=media_path)
-        stream = open('document.yaml', 'a')
-        yaml.dump(data, stream)
-
-    def multimedia_folder_hash_csv(self, input_path='../.temp/',
-                                   output_path='../.mmdb/object/',
-                                   save_path='../.mmdb/info/info.csv'):
-        """
-        檔案與雜湊值對應CSV.
-
-        將檔案雜湊後得出的雜湊值與檔名搭配後存入CSV檔
-        """
-        multimedias = HM.scan_folder(path=input_path)
-        for multimedia in multimedias:
-            HM.multimedia_hash(input_path=input_path + multimedia,
-                               output_path=output_path)
-
-            with open(save_path,
-                      mode='a', newline='', encoding='utf8') as csvfile:
-                # 建立 CSV 檔寫入器g
-                writer = csv.writer(csvfile)
-
-                # 寫入一列資料
-                writer.writerow([HM.hashfilename, HM.filename])
-
-    def multimedia_folder_hash_yaml(self, input_path='../.temp/',
-                                    output_path='../.mmdb/object/',
-                                    save_path='../.mmdb/info/info.csv'):
-        """
-        檔案、雜湊值與檔案資訊資料表.
-
-        將檔案雜湊後得出的雜湊值與檔名搭配後存入YAML。
-        """
-        multimedias = HM.scan_folder(path=input_path)
-        for multimedia in multimedias:
-            HM.multimedia_hash(input_path=input_path + multimedia,
-                               output_path=output_path)
-
-            with open(save_path,
-                      mode='a', newline='', encoding='utf8') as csvfile:
-                # 建立 CSV 檔寫入器g
-                writer = csv.writer(csvfile)
-
-                # 寫入一列資料
-                writer.writerow([HM.hashfilename, HM.filename])
-
 
 if __name__ == '__main__':
     MI = MultimediaInfo()
-    MI.read_file_info()
-    MI.read_folder_info()
     MI.multimedia_folder_hash_csv()
