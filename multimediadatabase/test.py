25ce84b40cd5542e62954db9eb14c37af55d2f20
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2020-01-07 00:21:45 +0800

    如果採用，此提交將新增測試函式，並簡化測試方式

--- multimediadatabase/test.py
+++ multimediadatabase/test.py
@@ -4,25 +4,23 @@
 多媒體資料庫主控台。
 """
 import yaml
-from .multimedia_database.multimedia_info import MultimediaInfo as mi
-from .multimedia_database.multimedia_summary import HashMultimedia as hm
-from .multimedia_database.multimedia_database import Insert
+from multimedia_database.multimedia_info import MultimediaInfo as mi
+from multimedia_database.multimedia_summary import HashMultimedia as hm
+from multimedia_database.multimedia_database import Insert
+from multimedia_database.multimedia_database import Create
 
-MI = mi()
-HM = hm()
-INSERT = Insert(mmdb_path='__mmdb__/', mpdlcache_path='__mpdlcache__/')
+NONE_MP4 = "894fad667b74fcff03aaa2f55cf933de1b9fd9663b1e5c0ff80fc6b44fa9542a"
+INFO_OBJECT = \
+    "0dbf40e4277f897c5a87a1bd32bedd75360df855623d8dfa12a9b7631ebc9c70"
 
 
 def test_multimedia_hash():
     """測試fun multimedia_hash."""
     # 測試檔案輸出與輸入是否一樣
-    HM.multimedia_hash(input_path='__mpdlcache__/None.mp4',
-                       output_path='__mmdb__/object/')
-    multimedia_hash_test = HM.hashfilename
-    HM.multimedia_hash(
-        input_path='__mmdb__/object/' + multimedia_hash_test,
-        output_path='__mmdb__/object/')
-    if multimedia_hash_test == HM.hashfilename:
+    hash_media = hm()
+    hash_media.multimedia_hash(input_path='__mpdlcache__/None.mp4',
+                               output_path='__mmdb__/object/')
+    if hash_media.hashfilename == NONE_MP4:
         assert True
 
 
@@ -30,7 +28,7 @@ def test_path_string_extraction():
     """
     測試fun path_string_extraction.
 
-    測試檔案輸出與輸入是否一樣
+    測試檔案名稱輸出與預想是否一樣。
     """
     text = 'abcd/efgh.mp4'
     test_list = [
@@ -55,7 +53,8 @@ def test_path_string_extraction():
 
 def test_read_file_info():
     """測試fun read_file_info."""
-    info_data = MI.read_file_info(path='__mpdlcache__/None.mp4')
+    multimedia = mi()
+    info_data = multimedia.read_file_info(path='__mpdlcache__/None.mp4')
     if isinstance(info_data, dict):
         print(yaml.dump(info_data))
         assert True
@@ -63,29 +62,40 @@ def test_read_file_info():
 
 def test_read_folder_info():
     """測試fun read_folder_info."""
-    info_data = MI.read_folder_info(path='__mpdlcache__/')
+    multimedia = mi()
+    info_data = multimedia.read_folder_info(path='__mpdlcache__/')
     if isinstance(info_data, list):
         print(yaml.dump(info_data))
         assert True
 
 
+def test_create_table_yaml():
+    """測試fun create_table_yaml."""
+    create = Create(
+        mmdb_path='__mmdb__/', mpdlcache_path='__mpdlcache__/')
+    create.create_table_yaml(name='Table.yaml')
+    hm.remove_file(path='__mmdb__/table/Table.yaml')
+    assert True
+
+
 def test_save_info_yaml():
     """測試fun save_info_yaml."""
-    HM.multimedia_hash(input_path='__mpdlcache__/None.mp4',
-                       output_path='__mmdb__/object/')
-    save_hash_name = INSERT.save_info_yaml(hash_value=HM.hashfilename,
+    insert = Insert(mmdb_path='__mmdb__/', mpdlcache_path='__mpdlcache__/')
+    hash_media = hm()
+    hash_media.multimedia_hash(input_path='__mpdlcache__/None.mp4',
+                               output_path='__mmdb__/object/')
+    save_hash_name = insert.save_info_yaml(hash_value=hash_media.hashfilename,
                                            media_path='__mpdlcache__/None.mp4')
-
-    HM.multimedia_hash(input_path='__mmdb__/object/' + save_hash_name,
-                       output_path='__mmdb__/object/')
-
-    if save_hash_name == HM.hashfilename:
+    if save_hash_name == INFO_OBJECT:
         assert True
+    else:
+        assert False
 
 
 def test_check_database():
     """check_database."""
-    multimedias = INSERT.check_database()
+    insert = Insert(mmdb_path='__mmdb__/', mpdlcache_path='__mpdlcache__/')
+    multimedias = insert.check_database()
     print(multimedias)
     if isinstance(multimedias, list):
         assert True
@@ -95,5 +105,6 @@ def test_check_database():
 
 def test_multimedia_folder_hash():
     """測試fun multimedia_folder_hash."""
-    INSERT.multimedia_folder_hash(table='Table.yaml')
+    insert = Insert(mmdb_path='__mmdb__/', mpdlcache_path='__mpdlcache__/')
+    insert.multimedia_folder_hash(table='Table.yaml')
     assert True
